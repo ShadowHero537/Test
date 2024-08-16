929b6745ee09cbbdfc466a493ee6d8b2cb26f817
ShadowHero537 <vegettoblackgamer002@gmail.com>
2024-08-16 08:47:25 -0300

    Aula de Versionamento

---
+++ (Meu Nome)-Versionamento_16_08_24.py
@@ -0,0 +1,5 @@
+#16/08/2024
+#Versionamento
+
+print("Olá, meu nome é:\nRhuan Fellipe")
+print("E eu sou um estudante de ensino técnico")
---
+++ ATV2. Versionamento_16_08_24_(2).py
@@ -0,0 +1,10 @@
+#16/08/2024
+#Versionamento
+
+
+nemero=''
+numero=int(input("Digite um numero de 1 a 10\n"))
+if numero>=1 and numero<=10:
+    print(f"Obrigado por escolher corretamente, aqui está o seu número: {numero}")
+elif numero<=0 and numero>=11:
+    print("Porque você é assim >:(")
